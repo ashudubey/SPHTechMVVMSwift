49dd96e2f6a91ecd28be9ae668b8abd988f7159c
Ashutosh Dubey <ashutosh.dubey@nagarro.com>
2019-01-29 11:04:32 +0530

    Network data parser

---
+++ SPHTech/SPHTech/Services+Manager/NetworkConstaants.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkConstaants.swift
+//  SPHTech
+//
+//  Created by Ashutosh Dubey on 29/01/19.
+//  Copyright © 2019 SPHTech. All rights reserved.
+//
+
+import Foundation
+
+//MARK:- Service enums
+enum Result<T, E: Error> {
+    case success(T)
+    case failure(E)
+}
+
+enum ErrorResult: Error {
+    case network(string: String)
+    case parser(string: String)
+    case custom(string: String)
+}
---
+++ SPHTech/SPHTech/Services+Manager/ParserHelper.swift
@@ -0,0 +1,10 @@
+//
+//  ParserHelper.swift
+//  SPHTech
+//
+//  Created by Ashutosh Dubey on 29/01/19.
+//  Copyright © 2019 SPHTech. All rights reserved.
+//
+
+import Foundation
+
