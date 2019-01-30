58bcb8f6299b1396724d0716167b359414ca0496
Ashutosh Dubey <ashutosh.dubey@nagarro.com>
2019-01-30 09:48:35 +0530

    Added UI skeleton

---
+++ SPHTech/SPHTech/YearlyMobileDataViewController.swift
@@ -0,0 +1,21 @@
+//
+//  YearlyMobileDataViewController.swift
+//  SPHTech
+//
+//  Created by Ashutosh Dubey on 29/01/19.
+//  Copyright © 2019 SPHTech. All rights reserved.
+//
+
+import UIKit
+
+class YearlyMobileDataViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Yearly Mobile Data Volume"
+    }
+
+}
+
